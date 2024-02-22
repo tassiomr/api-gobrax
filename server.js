2897d28432b297b056307b9bac6da6a33da48668
Tassio Rocha <tassiomarcosr@outlook.com>
2024-02-22 08:39:01 -0300

    chore: upgade api

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+    '/*': '/$1',
+}));
+
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
