277b76e7f0a36f28303f56fc6ee1f9de7a7b7eb6
Tassio Rocha <tassiomarcosr@outlook.com>
2024-02-22 08:35:31 -0300

    chore: upgade api

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-const router = jsonServer.router('db.json');
+const server = jsonServer.create()
 
-const middlewares = jsonServer.defaults();
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
 
-server.use(router);
+// Comment out to allow write operations
+const router = jsonServer.router('db.json')
 
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
-  "/*": "/$1",
-}));
-
-server.use(router);
-
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
 server.listen(3000, () => {
-  console.log('Running')
-});
+    console.log('JSON Server is running')
+})
 
-module.exports = server;
+// Export the Server API
+module.exports = server
